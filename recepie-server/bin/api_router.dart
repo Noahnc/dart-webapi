7a1c0662c5b00cc8ad9397053815131070970d9f
Noahnc <nc.canadea@gmail.com>
2022-08-22 12:36:45 +0200

    update api_handler

--- recepie-server/bin/api_router.dart
+++ recepie-server/bin/api_router.dart
@@ -2,6 +2,7 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'dart:convert';
 import '../models/recipe.dart';
+import 'db_handler.dart';
 
 /// Class used to set up all the routing for your server
 class Service {
@@ -9,27 +10,32 @@ class Service {
   Handler get handler {
     final router = Router();
 
-    router.get('/recipe/<id>', (Request request, int id) {
-      return Response.ok(id);
+    router.get('/recipe/<id>', (Request request, String id) {
+      var data = DBHandler.getInstance().getById(int.parse(id));
+      return Response.ok(jsonEncode(data));
     });
 
-    router.delete('/recipe/<id>', (Request request, int id) {
-      return Response.ok("Delete id: $id");
+    router.delete('/recipe/<id>', (Request request, String id) {
+      DBHandler.getInstance().deleteById(int.parse(id));
+      return Response.ok("ID $id deleted");
     });
 
     router.get('/recipe', (Request request) {
-      return Response.ok("Working");
+      var data = DBHandler.getInstance().getAll();
+      return Response.ok(jsonEncode(data));
     });
 
-    router.post('/recipe', (Request request) {
-        final body = request.readAsString();
-        return Response.ok(body);
+    router.post('/recipe', (Request request) async {
+      final body = await request.readAsString();
+      var recipe = Recipe.fromJson(jsonDecode(body));
+      DBHandler.getInstance().create(recipe);
+      return Response.ok(body);
     });
 
     // A catch all of the non implemented routing, useful for showing 404 page now found
     // With the server running try to open http://localhost:8080/abc
     router.all('/<ignored|.*>', (Request request) {
-      return Response.notFound('Page not found');
+      return Response.notFound('Route not found');
     });
 
     return router;
--- recepie-server/models/recipe.dart
+++ recepie-server/models/recipe.dart
@@ -1,4 +1,3 @@
-
 class Recipe {
   int id;
   String name;
@@ -7,10 +6,18 @@ class Recipe {
 
   Recipe(this.id, this.name, this.time, this.marked);
   factory Recipe.fromJson(dynamic json) {
-    return Recipe(json['age'] as int, json['name'] as String, json['time'] as int, json['marked'] as int);
+    return Recipe(json['id'] as int, json['name'] as String,
+        json['time'] as int, json['marked'] as int);
   }
   @override
   String toString() {
     return '{ ${this.id}, ${this.name}, ${this.time}, ${this.marked} }';
   }
+
+  Map toJson() => {
+        'id': id,
+        'name': name,
+        'time': time,
+        'marked': marked,
+      };
 }
